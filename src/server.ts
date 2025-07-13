2254d88a45e9ec008563c87247e7e0a3f48b22cd
João <contato@joaofelipegomes.com>
2025-07-13 12:51:47 -0300

    genia

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
@@ -10,12 +11,15 @@ import { postQuestionRoute } from './http/routes/create-question.ts'
 import { postRoomsRoute } from './http/routes/create-rooms.ts'
 import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { postAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -28,5 +32,6 @@ app.register(getRoomsRoute)
 app.register(postRoomsRoute)
 app.register(getRoomQuestionsRoute)
 app.register(postQuestionRoute)
+app.register(postAudioRoute)
 
 app.listen({ port: env.PORT })
